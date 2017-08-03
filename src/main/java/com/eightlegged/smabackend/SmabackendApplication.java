37e79958c61de8eec2801b20b387d0ed5e1b241a
RGKim <heehouse1@gmail.com>
2017-08-03 09:51:03 +0900

    First Commit

---
+++ src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
@@ -0,0 +1,12 @@
+package com.eightlegged.smabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmabackendApplication.class, args);
+	}
+}
