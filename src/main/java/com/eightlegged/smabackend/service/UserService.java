913080a0ced7ea98004b44317c9cc3d20822aac6
RGKim <heehouse1@gmail.com>
2017-08-03 10:09:18 +0900

    Make User Service

---
+++ src/main/java/com/eightlegged/smabackend/service/UserService.java
@@ -0,0 +1,10 @@
+package com.eightlegged.smabackend.service;
+
+import com.eightlegged.smabackend.entity.User;
+
+public interface UserService {
+	User findById(Long userId);
+	User findByEmail(String email);
+	User findByUsername(String username);
+	Boolean createUser(String username, String email, String password);
+}
