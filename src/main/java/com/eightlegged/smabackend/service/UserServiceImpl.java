1bbb458a485675f7eea674d3ec81020c114ffe71
RGKim <heehouse1@gmail.com>
2017-08-03 11:16:22 +0900

    Implement User Service

--- src/main/java/com/eightlegged/smabackend/service/UserServiceImpl.java
+++ src/main/java/com/eightlegged/smabackend/service/UserServiceImpl.java
@@ -1,5 +1,46 @@
 package com.eightlegged.smabackend.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.eightlegged.smabackend.entity.Role;
+import com.eightlegged.smabackend.entity.User;
+import com.eightlegged.smabackend.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User findByEmail(String email) {
+		User user = userRepository.findByEmail(email);
+		return user;
+	}
+
+	@Override
+	public User findByUsername(String username) {
+		User user = userRepository.findByUserName(username);
+		return user;
+	}
+
+	@Override
+	public Boolean createUser(String userName, String email, String password) {
+		if (findByEmail(email) == null && findByUsername(userName) == null) {
+			User user = new User();
+			user.setEmail(email);
+			user.setName(userName);
+			user.setPassword(password);
+			user.setRole(Role.USER);
+			userRepository.save(user);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public User findById(Long userId) {
+		return userRepository.findOne(userId);
+	}
 }
