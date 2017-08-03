759f794ad8f701f1b5413e5bb67d26ede04c4580
RGKim <heehouse1@gmail.com>
2017-08-03 10:47:03 +0900

    Implement Meeting Service

--- src/main/java/com/eightlegged/smabackend/service/MeetingServiceImpl.java
+++ src/main/java/com/eightlegged/smabackend/service/MeetingServiceImpl.java
@@ -1,5 +1,37 @@
 package com.eightlegged.smabackend.service;
 
-public class MeetingServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eightlegged.smabackend.entity.Meeting;
+import com.eightlegged.smabackend.entity.User;
+import com.eightlegged.smabackend.repository.MeetingRepository;
+
+@Service
+public class MeetingServiceImpl implements MeetingService{
+	
+	@Autowired
+    private MeetingRepository MeetingRepository;
+	
+	@Override
+	public Iterable<Meeting> list(User user) {
+		return MeetingRepository.findAllByUser(user);
+	}
+
+	@Override
+	public void createMeeting(Meeting meeting) {
+		MeetingRepository.save(meeting);
+	}
+
+	@Override
+	public void deleteMeetingById(long id) {
+		MeetingRepository.delete(id);
+	}
+
+	@Override
+	public Meeting findOne(long id) {
+		Meeting meeting = MeetingRepository.findOne(id);
+		return meeting;
+	}
 
 }
