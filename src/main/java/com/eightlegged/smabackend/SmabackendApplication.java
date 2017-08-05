a455f7d7ad42e57f4584ca36964b6df3f72d3f92
heeho <heeho@KSY-Lenovo>
2017-08-05 16:40:35 +0900

    Add author

--- src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
+++ src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
@@ -3,6 +3,14 @@ package com.eightlegged.smabackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Kim Sae-Young(heehouse1@gmail.com)
+ *
+ * @FileName SmabackendApplication.java
+ * @Project smabackend
+ * @Date 2017. 8. 5.
+ */
+
 @SpringBootApplication
 public class SmabackendApplication {
 
