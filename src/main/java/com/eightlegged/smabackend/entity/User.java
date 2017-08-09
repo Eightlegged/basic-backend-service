faabf25b12f2821d8e79bcd640d8a91a9f194e49
RGKim <heehouse1@gmail.com>
2017-08-10 08:20:45 +0900

    Simplify libraries

--- src/main/java/com/eightlegged/smabackend/entity/User.java
+++ src/main/java/com/eightlegged/smabackend/entity/User.java
@@ -4,17 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * @author Kim Sae-Young(heehouse1@gmail.com)
