40cfe826f4c1071739c9d5a661a33e1b3d9ef54a
RGKim <heehouse1@gmail.com>
2017-08-09 13:59:25 +0900

    Add Bean for JPA

--- src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
+++ src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
@@ -1,7 +1,11 @@
 package com.eightlegged.smabackend;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 /**
  * @author Kim Sae-Young(heehouse1@gmail.com)
@@ -16,5 +20,12 @@ public class SmabackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmabackendApplication.class, args);
+	}
+
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+		HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+		factory.setEntityManagerFactory(emf);
+		return factory;
 	}
 }
