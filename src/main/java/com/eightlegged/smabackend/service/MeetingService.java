2853020b86068a1093ded90af9a07d1f0ca51965
RGKim <heehouse1@gmail.com>
2017-08-10 14:14:57 +0900

    Method to create user list & meeting list(error remain)

--- src/main/java/com/eightlegged/smabackend/service/MeetingService.java
+++ src/main/java/com/eightlegged/smabackend/service/MeetingService.java
@@ -1,13 +1,16 @@
 package com.eightlegged.smabackend.service;
 
+import java.util.List;
+
 import com.eightlegged.smabackend.entity.Meeting;
-//import com.eightlegged.smabackend.entity.User;
+import com.eightlegged.smabackend.entity.User;
 
 public interface MeetingService {
-//	Iterable<Meeting> list(User user);
 	void createMeeting(Meeting Meeting);
 	void deleteMeetingById(long id);
 	void startMeeting(long id);
 	void completeMeeting(long id);
+	void setUser(long id) throws Exception;
+	List<User> userList(User user);
 	Meeting findOne(long id);
 }
--- src/main/java/com/eightlegged/smabackend/service/MeetingServiceImpl.java
+++ src/main/java/com/eightlegged/smabackend/service/MeetingServiceImpl.java
@@ -3,24 +3,38 @@ package com.eightlegged.smabackend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.eightlegged.smabackend.entity.Meeting;
 import com.eightlegged.smabackend.entity.Status;
+import com.eightlegged.smabackend.entity.User;
 import com.eightlegged.smabackend.repository.MeetingRepository;
+import com.eightlegged.smabackend.repository.UserRepository;
 
 @Service
-public class MeetingServiceImpl implements MeetingService{
-	
+public class MeetingServiceImpl implements MeetingService {
+
 	private static final Logger logger = LoggerFactory.getLogger(MeetingServiceImpl.class);
-	
+
 	@Autowired
-    private MeetingRepository MeetingRepository;
+	private MeetingRepository MeetingRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public void createMeeting(Meeting meeting) {
 		MeetingRepository.save(meeting);
+		try {
+			setUser(meeting.getId());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		logger.info("Meeting Created! Meeting ID: " + meeting.getId());
 	}
 
@@ -34,6 +48,7 @@ public class MeetingServiceImpl implements MeetingService{
 	public void startMeeting(long id) {
 		// TODO Auto-generated method stub
 		Meeting meeting = MeetingRepository.findOne(id);
+		MeetingRepository.delete(meeting);
 		meeting.setStatus(Status.START);
 		MeetingRepository.save(meeting);
 		logger.info("Meeting Started! Meeting ID: " + MeetingRepository.findOne(id).getId());
@@ -43,15 +58,55 @@ public class MeetingServiceImpl implements MeetingService{
 	public void completeMeeting(long id) {
 		// TODO Auto-generated method stub
 		Meeting meeting = MeetingRepository.findOne(id);
+		MeetingRepository.delete(meeting);
 		meeting.setStatus(Status.COMPLETE);
 		MeetingRepository.save(meeting);
 		logger.info("Meeting Finished! Meeting ID: " + MeetingRepository.findOne(id).getId());
 	}
-	
+
 	@Override
 	public Meeting findOne(long id) {
 		Meeting meeting = MeetingRepository.findOne(id);
 		return meeting;
 	}
 
+	@Override
+	public List<User> userList(User user) {
+		// TODO Auto-generated method stub
+		List<User> userList = new ArrayList<User>();
+		userList.add(user);
+		return null;
+	}
+
+	@Override
+	public void setUser(long id) throws Exception{
+		// TODO Auto-generated method stub
+		Meeting meeting = MeetingRepository.findOne(id);
+		for (int i = 0; i < meeting.getUserList().size(); i++) {
+			User user = userRepository.findByEmail(meeting.getUserList().get(i).getEmail());
+			List<Meeting> existList = user.getMeetingList();
+
+			
+
+			boolean save = true;
+			for (int j = 0; j < existList.size(); j++) {
+				if (id == existList.get(j).getId()) {
+					save = false;
+					break;
+				}
+			}
+
+			existList.add(meeting);
+			user.setMeetingList(existList);
+			
+			if (save == true) {
+				System.out.println(i);
+				userRepository.save(user);
+			}
+
+			logger.info("User(ID: " + user.getId() + ") Participate in Meeting(ID: "
+					+ MeetingRepository.findOne(id).getId() + ")");
+
+		}
+	}
 }
