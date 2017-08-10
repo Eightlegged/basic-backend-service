34817ce203d4dfd3c79b0c6c9419204b9daf09dc
RGKim <heehouse1@gmail.com>
2017-08-10 17:29:38 +0900

    Get Meeting List

--- src/main/java/com/eightlegged/smabackend/controller/UserController.java
+++ src/main/java/com/eightlegged/smabackend/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.eightlegged.smabackend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eightlegged.smabackend.entity.Meeting;
 import com.eightlegged.smabackend.entity.User;
 import com.eightlegged.smabackend.service.UserService;
 
@@ -33,6 +37,13 @@ public class UserController {
 		System.out.println("E-Mail: " + user.getEmail());
 		System.out.println("Pssword: " + user.getPassword());
 		return userservice.createUser(user.getUserName(), user.getEmail(),user.getPassword());
+	}
+	
+	@RequestMapping(
+			value="/user/{id}", method=RequestMethod.GET,
+			produces= {MediaType.APPLICATION_JSON_VALUE})
+	public List<Meeting> meetingList(@PathVariable Long id){
+		return userservice.findById(id).getMeetingList();
 	}
 	
 	@RequestMapping(
