4d793dcdcf2e5eae94c988e123fc422f347d3661
RGKim <heehouse1@gmail.com>
2017-08-14 17:58:03 +0900

    Cloud Deploy Setting

--- src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
+++ src/main/java/com/eightlegged/smabackend/SmabackendApplication.java
@@ -1,11 +1,7 @@
 package com.eightlegged.smabackend;
 
-import javax.persistence.EntityManagerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 /**
  * @author Kim Sae-Young(heehouse1@gmail.com)
@@ -21,11 +17,7 @@ public class SmabackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmabackendApplication.class, args);
 	}
+	
+	
 
-	@Bean
-	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
-		HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
-		factory.setEntityManagerFactory(emf);
-		return factory;
-	}
 }
