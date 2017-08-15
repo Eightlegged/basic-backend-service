b3e34847bf7caaa02d87d6ddcd87667a64e9348f
heeho <heeho@KSY-Lenovo>
2017-08-15 23:19:39 +0900

    CORS Setting

--- src/main/java/com/eightlegged/smabackend/controller/MeetingController.java
+++ src/main/java/com/eightlegged/smabackend/controller/MeetingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,18 +34,21 @@ public class MeetingController {
 	@Autowired
 	MeetingRepository meetingrepository;
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/add", method = RequestMethod.POST, consumes = {
 			MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public String createmeeting(@RequestBody Meeting meeting) {
 		return meetingservice.createMeeting(meeting);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/info/{id}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public Meeting meetinginfo(@PathVariable Long id) {
 
 		return meetingrepository.findOne(id);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/complete", method = RequestMethod.GET, produces = {
 			MediaType.APPLICATION_JSON_VALUE })
 	public List<Meeting> meetinginfo_c() {
@@ -52,6 +56,7 @@ public class MeetingController {
 		return meetingrepository.findByStatus(Status.COMPLETE);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/", method = RequestMethod.GET, produces = {
 			MediaType.APPLICATION_JSON_VALUE })
 	public List<Meeting> meetinginfo_w() {
@@ -59,40 +64,47 @@ public class MeetingController {
 		return meetingrepository.findByStatus(Status.WAIT);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/1", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<Meeting> meeting_part1() {
 
 		return meetingrepository.findByPartname(Partname.Architecture);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/2", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<Meeting> meeting_part2() {
 
 		return meetingrepository.findByPartname(Partname.Engineering);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/3", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<Meeting> meeting_part3() {
 
 		return meetingrepository.findByPartname(Partname.RnD);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/4", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<Meeting> meeting_part4() {
 
 		return meetingrepository.findByPartname(Partname.QA);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/delete/{id}", method = RequestMethod.POST)
 	public String deletemeeting(@PathVariable Long id) {
 		return meetingservice.deleteMeetingById(id);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/start/{id}", method = RequestMethod.POST)
 	public String startmeeting(@PathVariable Long id) {
 		return meetingservice.startMeeting(id);
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/meeting/end/{id}", method = RequestMethod.POST, consumes = {
 			MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public String completemeeting(@PathVariable Long id) {
--- src/main/java/com/eightlegged/smabackend/controller/UserController.java
+++ src/main/java/com/eightlegged/smabackend/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ public class UserController {
 	@Autowired
 	UserService userservice;
 	
+	@CrossOrigin
 	@RequestMapping(
 			value="/user/add", method=RequestMethod.POST, 
 			consumes= {MediaType.APPLICATION_JSON_VALUE},
@@ -39,6 +41,7 @@ public class UserController {
 		return userservice.createUser(user.getUserName(), user.getEmail(),user.getPassword());
 	}
 	
+	@CrossOrigin
 	@RequestMapping(
 			value="/user/all/{id}", method=RequestMethod.GET,
 			produces= {MediaType.APPLICATION_JSON_VALUE})
@@ -46,6 +49,7 @@ public class UserController {
 		return userservice.findById(id).getMeetingList();
 	}
 	
+	@CrossOrigin
 	@RequestMapping(
 			value="/user/wait/{id}", method=RequestMethod.GET,
 			produces= {MediaType.APPLICATION_JSON_VALUE})
@@ -53,6 +57,7 @@ public class UserController {
 		return userservice.meetingList_w(userservice.findById(id));
 	}
 	
+	@CrossOrigin
 	@RequestMapping(
 			value="/user/end/{id}", method=RequestMethod.GET,
 			produces= {MediaType.APPLICATION_JSON_VALUE})
@@ -60,6 +65,7 @@ public class UserController {
 		return userservice.meetingList_c(userservice.findById(id));
 	}
 	
+	@CrossOrigin
 	@RequestMapping(
 			value="/user/login",
 			method=RequestMethod.POST,
