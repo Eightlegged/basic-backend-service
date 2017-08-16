7b797198de7e77714551cbe9362630e92e273813
RGKim <heehouse1@gmail.com>
2017-08-16 22:14:37 +0900

    Create sample.sql

---
+++ sample.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE `base_info` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `user_info` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(255) NOT NULL,
+  `part` varchar(255) DEFAULT NULL,
+  `user_pw` varchar(255) DEFAULT NULL,
+  `user_auth` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UK_1d7p0xy0h7loanh4paykfxtna` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `meeting_serv` (
+  `meeting_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `mt_comment` varchar(255) DEFAULT NULL,
+  `mt_content` varchar(255) DEFAULT NULL,
+  `mt_date` date NOT NULL,
+  `mt_fn_time` time DEFAULT NULL,
+  `part` varchar(255) DEFAULT NULL,
+  `mt_st_time` time NOT NULL,
+  `mt_status` varchar(255) NOT NULL,
+  `mt_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`meeting_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `meeting_checklist` (
+  `meeting_meeting_id` bigint(20) NOT NULL,
+  `checklist` varchar(255) DEFAULT NULL,
+  KEY `FKbecyby7w5870yugo9nd9xtbjm` (`meeting_meeting_id`),
+  CONSTRAINT `FKbecyby7w5870yugo9nd9xtbjm` FOREIGN KEY (`meeting_meeting_id`) REFERENCES `meeting_serv` (`meeting_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `meeting_checklist` (
+  `meeting_meeting_id` bigint(20) NOT NULL,
+  `checklist` varchar(255) DEFAULT NULL,
+  KEY `FKbecyby7w5870yugo9nd9xtbjm` (`meeting_meeting_id`),
+  CONSTRAINT `FKbecyby7w5870yugo9nd9xtbjm` FOREIGN KEY (`meeting_meeting_id`) REFERENCES `meeting_serv` (`meeting_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
